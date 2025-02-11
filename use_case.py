f4f97e5483a76f791de1f7e8702fd231ff0cf825
prateek jain <jainprateekjp13@gmail.com>
2025-02-11 11:16:56 +0530

    [Prateek] Add: Python script for validating last name

--- use_case.py
+++ use_case.py
@@ -1,28 +1,31 @@
 import re
 import logging
 
-logging.basicConfig(filename='UC1_log_file.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='UC2_log_file.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def validate_first_name(name):
-    logging.debug(f"Received input for validation: '{name}'")
+def validate_name(name,name_type):
+    logging.debug(f"Received input for {name_type} validation: '{name}'")
     if len(name)<3 and name[0].isupper():
-        message=(f"Invalid First Name: '{name}' - It should have atleast 3 characters.")
+        message=(f"Invalid {name_type}: '{name}' - It should have at least 3 characters.")
         logging.error(message)
         print(message)
     elif len(name)>=3 and not name[0].isupper():
-        message=(f"Invalid First Name: '{name}' - The first letter should be capital.")
+        message=(f"Invalid {name_type}: '{name}' - The first letter should be capital.")
         logging.error(message)
         print(message)
     elif len(name)<3 and not name[0].isupper():
-        message=(f"Invalid First Name: '{name}' - The first letter should be capital and it should also have atleast 3 characters.")
+        message=(f"Invalid {name_type}: '{name}' - The first letter should be capital and it should also have at least 3 characters.")
         logging.critical(message)
         print(message)
     else:
-        message=(f"Valid First Name: '{name}'")
+        message=(f"Valid {name_type}: '{name}'")
         logging.info(message)
         print(message)
 
-name=input("Enter first name: ")
-validate_first_name(name)
+first_name=input("Enter first name: ")
+validate_name(first_name,"first name")
+
+last_name=input("Enter last name: ")
+validate_name(last_name,"last name")
 
 
